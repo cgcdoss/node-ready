b38b6a02fcbbcddcb75fcb4c9bc404fce04398e4
cgcdoss <gabi04061997@gmail.com>
2022-09-15 17:44:08 -0300

    fix: usando arrow function para não ter falha ao usar this
    Declarando como função acaba criando um contexto diferente do atual, e causa erro ao usar o this

--- src/controllers/example.controller.ts
+++ src/controllers/example.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 class ExampleController {
 
-  public async getDefault(req: Request, res: Response): Promise<Response> {
+  public getDefault = async (req: Request, res: Response): Promise<Response> => {
     return res.status(200).json({ msg: 'Hello world' });
   }
 
